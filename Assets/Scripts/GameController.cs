952b461d82036cf16725b95cb187959bbeb6778e
MadsChris92 <madschris888@gmail.com>
2015-07-08 19:46:42 +0200

    kjhguf

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,10 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour {
 	public Text life;
 	public int lives = 3;
+	float timePassed;
+	public Spawner spawner;
+	public int minTime = 5;
+	public int maxTime =10;
 	// Use this for initialization
 	void Start () {
 	
@@ -12,8 +16,16 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		timePassed += Time.deltaTime;
 		string liv = " " + lives;
 		life.text = liv;
+
+		if (timePassed > minTime && timePassed < maxTime) {
+			spawner.pathModes = Spawner.SpawnPaths.easyPath;
+			spawner.maxCurrentSpawn = 5;
+		} else {
+			spawner.pathModes = Spawner.SpawnPaths.Waiting;		}
+		Debug.Log (timePassed);
 	}
 
 	public void subractLife(){
