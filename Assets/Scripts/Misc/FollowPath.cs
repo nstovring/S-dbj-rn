ff10335794dbc0268cce09fd186151f1cdd0cd61
MadsChris92 <madschris888@gmail.com>
2015-07-08 12:50:41 +0200

    scene02 og misc

---
+++ Assets/Scripts/Misc/FollowPath.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class FollowPath : MonoBehaviour 
+{
+	public enum FollowType
+	{
+		MoveToward, Lerp
+	}
+
+	public FollowType Type = FollowType.MoveToward;
+	public p Path;
+	public float Speed = 1;
+	public float MaxDistanceToGoal = 0.1f;
+
+	private IEnumerator<Transform>_currentPoint;
+
+	public void Start()
+	{
+		if (Path == null) 
+		{
+			Debug.LogError("Path cannot be null", gameObject);
+			return;
+		}
+		_currentPoint = Path.GetPathEnumerator();
+		_currentPoint.MoveNext ();
+
+		if (_currentPoint.Current == null) {
+			return;
+				}
+		transform.position = _currentPoint.Current.position;
+	}
+	public void Update()
+	{
+		if (_currentPoint == null || _currentPoint.Current == null) {
+			return;
+				}
+		if (Type == FollowType.MoveToward) {
+			transform.position = Vector3.MoveTowards(transform.position, _currentPoint.Current.position, Time.deltaTime * Speed);
+				}
+		else if (Type == FollowType.Lerp) {
+			transform.position = Vector3.MoveTowards(transform.position, _currentPoint.Current.position, Time.deltaTime * Speed);
+		}
+
+		var distanceSquared = (transform.position - _currentPoint.Current.position).sqrMagnitude;
+		if (distanceSquared < MaxDistanceToGoal * MaxDistanceToGoal) {
+			_currentPoint.MoveNext();
+				}
+		}
+
+}
---
+++ Assets/Scripts/Misc/PlayerControl.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+	public GameObject Player;
+	public float gravity = 20;
+	public float speed = 8;
+	public float acceleration = 30;
+	public float jumpHeight = 12;
+	public float fireRate;
+
+	private float currentspeed;
+	private float targetSpeed;
+	private Vector2 amountToMove;
+
+	public GameObject Bullet;
+	public Transform bulletSpawn;
+
+	private BulletPhysics bulletPhysics;
+	private PlayerPhysics playerPhysics;
+	float direct;
+
+	// Use this for initialization
+	void Start () {
+
+		bulletPhysics = GetComponent <BulletPhysics> ();
+		playerPhysics = GetComponent <PlayerPhysics> ();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 playerPos = Player.transform.position;
+		Debug.Log (playerPos);
+	
+		targetSpeed = Input.GetAxisRaw ("Horizontal") * speed;
+		currentspeed = IncrementTowards (currentspeed, targetSpeed, acceleration);
+
+		if (playerPhysics.grounded) {
+			amountToMove.y= 0;
+			if(Input.GetButtonDown("Jump")){
+
+				amountToMove.y = jumpHeight;
+
+			}
+				}
+
+		if (playerPhysics.atWall) {
+			targetSpeed =0;
+			currentspeed = 0;
+				}
+
+		amountToMove.x = currentspeed;
+		amountToMove.y -= gravity *Time.deltaTime;
+		playerPhysics.Move (amountToMove * Time.deltaTime);
+		direct = playerPhysics.getDirect(10);
+
+	
+
+
+
+		float nextFire = 0.0f;
+		if (Input.GetButtonDown ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Shoot ();
+
+				}
+	
+		}
+		
+
+	private void Shoot(){
+
+		GameObject clone = Instantiate (Bullet, bulletSpawn.position, bulletSpawn.rotation)as GameObject;
+		if (direct > 0) {
+						clone.rigidbody.AddForce (clone.transform.right * 1500);
+				} else {
+			clone.rigidbody.AddForce (clone.transform.up * 1500);
+				}
+		Destroy (clone, 2);
+	
+		}
+
+
+		private float IncrementTowards(float n, float target, float a)
+		{
+			if (n == target){
+				return n;
+			}
+			else {
+				float dir = Mathf.Sign(target - n);
+				n += a * Time.deltaTime * dir;
+				return (dir == Mathf.Sign(target-n))? n: target;
+
+			}
+		}
+	
+}
