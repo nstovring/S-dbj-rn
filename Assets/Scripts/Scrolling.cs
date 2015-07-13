2435a611296f34addf1c8136b84584b2c749182e
MadsChris92 <madschris888@gmail.com>
2015-07-13 14:46:49 +0200

    dasfaf

--- Assets/Scripts/Scrolling.cs
+++ Assets/Scripts/Scrolling.cs
@@ -3,15 +3,49 @@ using System.Collections;
 
 public class Scrolling : MonoBehaviour {
 
+	public enum layer {
+		ground, sky, top
+	}
+
+	public layer layers;
 	bool instantiated= true;
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
 	void Update () {
+		if (layers == layer.ground){
+			bottom ();
+		}
+		if (layers == layer.sky){
+			sky ();
+		}
+		if (layers == layer.top){
+			top();
+		}
+	}
+
+	void bottom(){
 		transform.position -= new Vector3(0,Time.deltaTime,0);
+		if(transform.position.y <= -12 && instantiated){
+			Instantiate(gameObject, new Vector3(0,27,0),Quaternion.identity);
+			instantiated = false;
+		}
+	}
+	void sky(){
+		transform.position -= new Vector3(0,Time.deltaTime*5,0);
+		gameObject.GetComponentInChildren<SpriteRenderer>().color = new Color (1f,1f,1f,.5f);
+		if(transform.position.y <= -12 && instantiated){
+			Instantiate(gameObject, new Vector3(0,27,0),Quaternion.identity);
+			gameObject.GetComponentInChildren<SpriteRenderer>().color = new Color (1f,1f,1f,.5f);
+			instantiated = false;
+			//SpriteRenderer.color = new Color(1f,1f,1f,.5f);
+		
+		}
+	}
+
+	void top (){
+		transform.position -= new Vector3(0,Time.deltaTime*10,0);
 		if(transform.position.y <= -12 && instantiated){
 			Instantiate(gameObject, new Vector3(0,27,0),Quaternion.identity);
 			instantiated = false;
