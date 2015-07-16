cbde2a82f56e76796e4329d6ca7fca11240b6657
MadsChris92 <madschris888@gmail.com>
2015-07-16 13:54:46 +0200

    grsg

---
+++ Assets/Button.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+
+	public Sprite[] image = new Sprite[2];
+	SpriteRenderer spriterend;
+	// Use this for initialization
+	void Start () {
+		spriterend = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	spriterend.sprite = image [0];	
+	}
+}
